a32ef22d4c8288092cb203457159a521f1437788
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2017-08-04 12:07:38 -0300

    Read each FASTA, then extract each gene sequence

--- Utils/sequenceExtractor.py
+++ Utils/sequenceExtractor.py
@@ -3,6 +3,7 @@ __author__      = "Henrique Frajacomo"
 
 import argparse
 from Bio import SeqIO
+import sys
 
 parser = argparse.ArgumentParser(description='Returns a fasta with aligned subsequences', add_help=True)
 parser.add_argument('-t','--tabular', dest='tabular', metavar='inputFile', type=str, help='File containing start and end position', required=True)
@@ -12,34 +13,31 @@ args = parser.parse_args()
 argTable = args.tabular
 sequenceFile = args.fasta
 
-
 repeat = False
-fullList = []
 splitList = []
-dataTable = open(argTable)
+
 resultFile = open("ResultFile.fasta", "w")
 
-for line in dataTable:
-	if(repeat):
- 		fullList.append(line.strip('\n'))
-	repeat = True
-
-StartIterator = SeqIO.parse(sequenceFile, "fasta")
 Iterator = SeqIO.parse(sequenceFile, "fasta")
-
-for element in fullList:
-	Iterator = StartIterator
-	splitList = element.split('\t')
-	for record in Iterator:
+numberOfGenes=0
+numberOfSeqs=0
+for record in Iterator:
+	numberOfSeqs=numberOfSeqs+1
+	dataTable = open(argTable)
+	for line in dataTable:
+		splitList = line.split('\t')
 		if(splitList[11] == record.id):
 			start = int(splitList[12])
 			end = int(splitList[13])
+			numberOfGenes=numberOfGenes+1
 			if(splitList[14]=="plus"):
 				resultFile.write(">" + splitList[2] + "\t" + splitList[14] +"\n" + str(record.seq[start:end]) + "\n")
-				print("Plus")   # Test only
 			else:
 				resultFile.write(">" + splitList[2] + "\t" + splitList[14] +"\n" + str((record.seq[start:end])[::-1]) + "\n")  #[::-1] -> reads the entry backwards
-				print("Minus")  # Test only 
+	dataTable.close()
 
-dataTable.close()
+print("There are ", numberOfGenes, " genes in the input TAB file! Yeahhh")
+print("There are ", numberOfSeqs, " sequences in the FASTA file! Yeahhh")
+
+Iterator.close()
 resultFile.close()
