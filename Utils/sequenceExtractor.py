cbea72d87fcc8b6cb2733b44f0819b0b58ff9e60
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2017-08-04 16:40:24 -0300

    Fixed script to retrieve gene sequences from both DNA strands given an input FASTA and a tabular gene file from NCBI; included a argument option for output FASTA

--- Utils/sequenceExtractor.py
+++ Utils/sequenceExtractor.py
@@ -3,20 +3,21 @@ __author__      = "Henrique Frajacomo"
 
 import argparse
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
 import sys
 
 parser = argparse.ArgumentParser(description='Returns a fasta with aligned subsequences', add_help=True)
 parser.add_argument('-t','--tabular', dest='tabular', metavar='inputFile', type=str, help='File containing start and end position', required=True)
-parser.add_argument('-f','--fasta', dest='fasta', metavar='inputFile', type=str, help='Fasta containing the entire sequence', required=True)
+parser.add_argument('-f','--fasta', dest='genomeFasta', metavar='inputFile', type=str, help='Fasta containing the entire sequence', required=True)
+parser.add_argument('-o','--out', dest='geneFasta', metavar='outputFile', type=str, help='Fasta containing gene sequences', required=True)
 
 args = parser.parse_args()
 argTable = args.tabular
-sequenceFile = args.fasta
+sequenceFile = args.genomeFasta
+resultFile = args.geneFasta
 
-repeat = False
-splitList = []
-
-resultFile = open("ResultFile.fasta", "w")
+resultFileOBJ = open(resultFile, "w")
 
 Iterator = SeqIO.parse(sequenceFile, "fasta")
 numberOfGenes=0
@@ -24,20 +25,24 @@ numberOfSeqs=0
 for record in Iterator:
 	numberOfSeqs=numberOfSeqs+1
 	dataTable = open(argTable)
-	for line in dataTable:
-		splitList = line.split('\t')
+	for gene in dataTable:
+		splitList = gene.split('\t')
 		if(splitList[11] == record.id):
-			start = int(splitList[12])
+			start = int(splitList[12])-1
 			end = int(splitList[13])
 			numberOfGenes=numberOfGenes+1
+			seq_description=splitList[2] + " [start:" + splitList[12] + "] [end:" + splitList[13] + "] [chrom:" + splitList[10] + "] [strand:" + splitList[14] +"]"
 			if(splitList[14]=="plus"):
-				resultFile.write(">" + splitList[2] + "\t" + splitList[14] +"\n" + str(record.seq[start:end]) + "\n")
+				generecord = SeqRecord(Seq(str(record.seq[start:end])),id=splitList[2],description=seq_description)
+				SeqIO.write(generecord,resultFileOBJ,"fasta")
 			else:
-				resultFile.write(">" + splitList[2] + "\t" + splitList[14] +"\n" + str((record.seq[start:end])[::-1]) + "\n")  #[::-1] -> reads the entry backwards
+				sequence=Seq(str(record.seq[start:end]))
+				generecord = SeqRecord(sequence.reverse_complement(),id=splitList[2],description=seq_description)
+				SeqIO.write(generecord,resultFileOBJ,"fasta")
 	dataTable.close()
 
-print("There are ", numberOfGenes, " genes in the input TAB file! Yeahhh")
-print("There are ", numberOfSeqs, " sequences in the FASTA file! Yeahhh")
+print("There are " + str(numberOfGenes) + " genes in the input TAB file! Yeahhh")
+print("There are " + str(numberOfSeqs) + " sequences in the FASTA file! Yeahhh")
 
 Iterator.close()
-resultFile.close()
+resultFileOBJ.close()
