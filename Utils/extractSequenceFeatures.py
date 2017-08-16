aad5229fc0edb8a64c63bdcf034fe35fcd00cec2
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2017-08-16 17:32:08 -0300

    Started writing a script to extract features from microRNA-mRNA interaction

---
+++ Utils/extractSequenceFeatures.py
@@ -0,0 +1,24 @@
+import Bio
+import numpy
+import argparse
+import re
+import orf_finder
+
+parser = argparse.ArgumentParser(description='Extracts features of provided sequences (GFF | FASTA)', add_help=True)
+parser.add_argument('-g','--gff', dest='gff', metavar='inputGFF', type=str, help='GFF containing information about features in genome', required=True)
+
+args = parser.parse_args()
+
+
+
+InputGFF = open(args.gff, "r")
+
+for line in InputGFF:
+	p = re.compile('#')
+ 	if p.match(line):
+		continue
+
+	splitList = line.split('\t')
+
+	if (splitList[2] == 'gene'):
+		print(line)
