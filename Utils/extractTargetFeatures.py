aa1ef1734aba2630b15a2c30491d660c4832eaa7
HFrajacomo <henrique.frajacomo@gmail.com>
2017-08-23 11:55:06 -0300

    Update extractTargetFeatures.py

--- Utils/extractTargetFeatures.py
+++ Utils/extractTargetFeatures.py
@@ -4,7 +4,7 @@ import argparse
 import re
 import orf_finder
 
-parser = argparse.ArgumentParser(description='Extracts features of provided sequences (GFF | FASTA)', add_help=True)
+parser = argparse.ArgumentParser(description='Extracts features of target sequences (GFF | FASTA)', add_help=True)
 parser.add_argument('-g','--gff', dest='gff', metavar='inputGFF', type=str, help='GFF containing information about features in genome', required=True)
 
 args = parser.parse_args()
